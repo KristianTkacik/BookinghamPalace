7c651d19ca73338199b7f7974015fa70fd6f981c
Kristian Tkacik <xtkacik3@fi.muni.cz>
2021-12-01 15:13:18 +0100

    Add customer service skeleton

---
+++ customer-service/src/test/java/cz/muni/fi/pv217/NativeCustomerResourceIT.java
@@ -0,0 +1,9 @@
+package cz.muni.fi.pv217;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeCustomerResourceIT extends CustomerResourceTest {
+
+    // Execute the same tests but in native mode.
+}
