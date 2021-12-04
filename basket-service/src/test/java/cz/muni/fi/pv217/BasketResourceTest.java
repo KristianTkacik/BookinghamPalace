2d07fbd4cefec3b1d982f63c780d06c26e4a9687
Kristian Tkacik <xtkacik3@fi.muni.cz>
2021-12-04 11:59:25 +0100

    Add testGetCustomerBasketEndpoint to basket service tests

--- basket-service/src/test/java/cz/muni/fi/pv217/BasketResourceTest.java
+++ basket-service/src/test/java/cz/muni/fi/pv217/BasketResourceTest.java
@@ -1,5 +1,7 @@
 package cz.muni.fi.pv217;
 
+import io.quarkus.test.common.QuarkusTestResource;
+import io.quarkus.test.h2.H2DatabaseTestResource;
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
@@ -7,15 +9,15 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
+@QuarkusTestResource(H2DatabaseTestResource.class)
 public class BasketResourceTest {
 
     @Test
-    public void testHelloEndpoint() {
+    public void testGetCustomerBasketEndpoint() {
         given()
-          .when().get("/hello")
-          .then()
-             .statusCode(200)
-             .body(is("Hello RESTEasy"));
+                .when().get("/customer/1/basket")
+                .then()
+                .statusCode(200)
+                .body(is("{\"id\":1,\"customerId\":1,\"items\":[]}"));
     }
-
 }
