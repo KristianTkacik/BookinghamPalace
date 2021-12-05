0319789474030cd89b147defc377088b9d278b8b
Kristian Tkacik <xtkacik3@fi.muni.cz>
2021-12-05 12:33:29 +0100

    Add tests to customer service

---
+++ customer-service/src/main/resources/import.sql
@@ -0,0 +1,31 @@
+INSERT INTO customer(id, name, email, passwordHash)
+VALUES (
+           nextval('hibernate_sequence'),
+           'Chuck',
+           'chuck@mail.com',
+           '$2a$04$UAe91i4Fc0n9LILx/Uejme9XhJXGu1oFha2TZ55DQHjqA0o4oRqPS'
+        );
+
+INSERT INTO customer(id, name, email, passwordHash)
+VALUES (
+           nextval('hibernate_sequence'),
+           'Alice',
+           'alice@mail.com',
+           '$2a$04$UAe91i4Fc0n9LILx/Uejme9XhJXGu1oFha2TZ55DQHjqA0o4oRqPS'
+       );
+
+INSERT INTO customer(id, name, email, passwordHash)
+VALUES (
+           nextval('hibernate_sequence'),
+           'John',
+           'john@mail.com',
+           '$2a$04$UAe91i4Fc0n9LILx/Uejme9XhJXGu1oFha2TZ55DQHjqA0o4oRqPS'
+       );
+
+INSERT INTO customer(id, name, email, passwordHash)
+VALUES (
+           nextval('hibernate_sequence'),
+           'Eve',
+           'eve@mail.com',
+           '$2a$04$UAe91i4Fc0n9LILx/Uejme9XhJXGu1oFha2TZ55DQHjqA0o4oRqPS'
+       );
