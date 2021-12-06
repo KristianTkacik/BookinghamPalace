23566eaf084f591d31fc91bd42ac71ea7f502923
Kristian Tkacik <xtkacik3@fi.muni.cz>
2021-12-06 17:15:51 +0100

    Fix small issue with IDs in import.sql in catalog service

--- catalog-service/src/main/resources/import.sql
+++ catalog-service/src/main/resources/import.sql
@@ -1,4 +1,4 @@
-INSERT INTO author(id, dateofbirth, name) VALUES (1, '1991-04-01', 'well known author');
+INSERT INTO author(id, dateofbirth, name) VALUES (nextval('hibernate_sequence'), '1991-04-01', 'well known author');
 
-INSERT INTO book(id, title, genre, price, releaseDate, author_id) VALUES (2, 'book-a', 0, 19.99, '2014-02-05', 1);
-INSERT INTO book(id, title, genre, price, releaseDate) VALUES (3, 'book-b', 1, 9.99, '2018-03-04');
+INSERT INTO book(id, title, genre, price, releaseDate, author_id) VALUES (nextval('hibernate_sequence'), 'book-a', 0, 19.99, '2014-02-05', 1);
+INSERT INTO book(id, title, genre, price, releaseDate) VALUES (nextval('hibernate_sequence'), 'book-b', 1, 9.99, '2018-03-04');
