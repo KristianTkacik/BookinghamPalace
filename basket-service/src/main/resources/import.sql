c9259e9891199cf1c8ad14af52d7f91c92bc0078
Kristian Tkacik <xtkacik3@fi.muni.cz>
2021-12-06 10:58:18 +0100

    Minor consistency change in import.sql in basket service

--- basket-service/src/main/resources/import.sql
+++ basket-service/src/main/resources/import.sql
@@ -4,4 +4,4 @@ INSERT INTO basket(id, customerId) VALUES (nextval('hibernate_sequence'), 3);
 INSERT INTO basket(id, customerId) VALUES (nextval('hibernate_sequence'), 4);
 
 INSERT INTO basketitem(id, basketId, bookId, bookTitle, quantity, unitPrice)
-VALUES (nextval('hibernate_sequence'), 1, 1, 'TestBook', 1, 20);
+VALUES (nextval('hibernate_sequence'), 1, 2, 'book-a', 1, 19.99);
