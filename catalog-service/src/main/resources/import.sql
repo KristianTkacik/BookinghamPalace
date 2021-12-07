048417f4608a0cc218196f8a44f3bc1e3a1a54d4
Radoslav Chudovsky <469069@mail.muni.cz>
2021-12-07 03:17:42 +0100

    Update book resource tests

--- catalog-service/src/main/resources/import.sql
+++ catalog-service/src/main/resources/import.sql
@@ -1,4 +1,15 @@
-INSERT INTO author(id, dateofbirth, name) VALUES (nextval('hibernate_sequence'), '1991-04-01', 'well known author');
+INSERT INTO author(id, dateofbirth, name) VALUES (nextval('hibernate_sequence'), '1991-04-01', 'author-a');
+INSERT INTO author(id, dateofbirth, name) VALUES (nextval('hibernate_sequence'), '2000-05-07', 'author-b');
 
 INSERT INTO book(id, title, genre, price, releaseDate, author_id) VALUES (nextval('hibernate_sequence'), 'book-a', 0, 19.99, '2014-02-05', 1);
 INSERT INTO book(id, title, genre, price, releaseDate) VALUES (nextval('hibernate_sequence'), 'book-b', 1, 9.99, '2018-03-04');
+INSERT INTO book(id, title, genre, price, releaseDate) VALUES (nextval('hibernate_sequence'), 'book-c', 1, 9.99, '2018-03-04');
+
+INSERT INTO author(id, dateofbirth, name) VALUES (nextval('hibernate_sequence'), '2000-05-07', 'author-c');
+INSERT INTO book(id, title, genre, price, releaseDate) VALUES (nextval('hibernate_sequence'), 'book-c', 1, 9.99, '2018-03-04');
+
+INSERT INTO author(id, dateofbirth, name) VALUES (nextval('hibernate_sequence'), '2000-05-07', 'author-f');
+INSERT INTO book(id, title, genre, price, releaseDate, author_id) VALUES (nextval('hibernate_sequence'), 'book-f', 1, 9.99, '2018-03-04', 8);
+
+
+
